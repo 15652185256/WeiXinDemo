7fd114ebb39e6c9ca9228bb9478bc12e7fe2d938
15652185256 <15652185256@163.com>
2016-05-18 14:20:15 +0800

    通道收到消息事件处理

--- WeiXinDemo/AppDelegate.swift
+++ WeiXinDemo/AppDelegate.swift
@@ -9,10 +9,109 @@
 import UIKit
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, XMPPStreamDelegate {
 
     var window: UIWindow?
+    
+    //通道
+    var xs : XMPPStream?
+    //服务器是否开启
+    var isOPen = false
+    //密码
+    var pwd = ""
+    
+    //状态代理
+    var ztdl : ZhuangTaiDelegate?
+    
+    //消息代理
+    var xxdl : XiaoXiDelegate?
+    
+    //收到消息
+    func xmppStream(sender: XMPPStream!, didSendMessage message: XMPPMessage!) {
+        
+        print(message)
+        
+        //如果是聊天消息
+        if message.isChatMessage() {
+            var msg = WXMessage()
+            //对消息内容进行判断 对方正在输入
+            if message.elementForName("composing") != nil {
+                msg.isComposing = true
+            }
+            //离线消息
+            if message.elementForName("delay") != nil {
+                msg.isDelay = true
+            }
+            //消息正文
+            if let body = message.elementForName("body") {
+                msg.body = body.stringValue()
+            }
+            //完整用户名
+            msg.from = message.from().user + "@" + message.from().domain
+            
+            //加入消息代理中
+            xxdl?.newMsg(msg)
+        }
+    }
+    
+    
+    //收到状态
+    func xmppStream(sender: XMPPStream!, didReceivePresence presence: XMPPPresence!) {
+        //我自己的用户名
+        let myUser = sender.myJID.user
+        //好友的用户名
+        let user = presence.from().user
+        //好友所在的域
+        let domain = presence.from().domain
+        //状态类型
+        let pType = presence.type()
+        //如果状态不是自己的
+        if (user != myUser) {
+            //状态保存的结构 
+            var zt = ZhuangTai()
+            //保存了状态的完整用户名
+            zt.name = user + "@" + domain
 
+            //上线
+            if pType == "availabel" {
+                zt.isOnline = true
+                ztdl?.isOn(zt)
+            }else if pType == "unavailabel" {
+                ztdl?.isOff(zt)
+            }
+        }
+        
+    }
+    
+    
+    //连接成功
+    func xmppStreamDidConnect(sender: XMPPStream!) {
+        isOPen = true
+        //验证密码
+        do {
+            try xs?.authenticateWithPassword(pwd)
+        } catch {
+            fatalError("密码错误")
+        }
+    }
+    
+    //验证成功
+    func xmppStreamDidAuthenticate(sender: XMPPStream!) {
+        //上线
+        goOnline()
+    }
+    
+    //建立通道
+    func buildStream()  {
+        xs = XMPPStream()
+        xs?.addDelegate(self, delegateQueue: dispatch_get_main_queue())
+    }
+    
+    //上线
+    func goOnline() {
+        
+    }
+    
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         
--- WeiXinDemo/Model/WXMessage.swift
+++ WeiXinDemo/Model/WXMessage.swift
@@ -11,15 +11,15 @@ import Foundation
 
 //好友消息结构
 struct WXMessage {
-    var body : String //内容
-    var from : String //来源
-    var isComposing : Bool //是否正在输入
-    var isDelay : Bool //是否离线
-    var isMe : Bool //是否是我发的
+    var body = "" //内容
+    var from = "" //来源
+    var isComposing = false //是否正在输入
+    var isDelay = false     //是否离线
+    var isMe = false        //是否是我发的
 }
 
 //状态结构
 struct ZhuangTai {
-    var name : String     //谁
-    var isOnline : Bool   //在线状态
+    var name = ""     //谁
+    var isOnline = false   //在线状态
 }
